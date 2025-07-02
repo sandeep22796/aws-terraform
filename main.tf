fed50ff77f88e1e29848e8d88a29c62f74c312c7
Sandeep Mahato <sandeep.stmarys@gmail.com>
2025-07-02 08:57:38 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -80,27 +80,8 @@ resource "aws_s3_object" "bucket-data2" {
   #key is the name of the file in the S3 bucket and in this case it will create a new folder
   # and put the file in that folder
 }
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.0.0"
-    }
-  }
+
+output "output-for-random_id" {
+  value = random_id.r_id.hex  
 }
 
-provider "aws" {
-  # Configuration options
- region = "eu-north-1"
-}
-
-resource "aws_instance" "mytestserver2" {
-  ami = "ami-0989038dff76173d3"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "mytestserver2"
-  }
-}
-
-  
